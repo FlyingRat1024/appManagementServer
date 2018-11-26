3cf89964d273721536fce98ba90b460a9bf800fe
Myh <737200833@qq.com>
2018-11-26 23:33:21 +0800

    update warehouse internal func

--- internal/warehouse/store_db.go
+++ internal/warehouse/store_db.go
@@ -1,9 +1,9 @@
 package warehouse
 
 func CreateInWarehouseTable(table *InWarehouseTableBody) error {
-
+	return nil
 }
 
 func CreateOutWarehouseTable(table *OutWarehouseTableBody) error {
-
+	return nil
 }
